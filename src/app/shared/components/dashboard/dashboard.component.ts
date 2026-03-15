96ae9f66e170bfcdaf8cc03140d71159235b9b55
Kareem Khalifa <karemkhalifaa@gmail.com>
2026-03-15 15:43:12 +0200

    feat: update dashboard metrics and improve appointment data handling

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { DoctorsService } from '../../../core/services/doctors.service';
 import { PatientService } from '../../../core/services/patient.service';
@@ -8,13 +8,11 @@ import { Appointment } from '../../../models/appointment';
 import { Patient } from '../../../models/patient';
 import { Doctor } from '../../../models/doctor';
 import { forkJoin, of, switchMap } from 'rxjs';
-
 interface AppointmentRow {
   appointment: Appointment;
   patient: Patient;
   doctor: Doctor;
 }
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -22,42 +20,44 @@ interface AppointmentRow {
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   constructor(
     public _DoctorsService: DoctorsService,
     public _PatientService: PatientService,
     private _Router: Router,
     private _AppointmentService: AppointmentService,
   ) {}
-
   doctorsCount!: number;
   patientsCount!: number;
   appointmentsCount!: number;
-  todayRows: AppointmentRow[] = [];
-
-  todayDate = dayjs().format('dddd, DD MMMM YYYY');
+  todayAppointments: Appointment[] = [];
+  patient!: Patient;
+  doctor!: Doctor;
+  todayRows!: AppointmentRow[];
+  todayDate = dayjs().format('dddd,DD MMMM YYYY');
   td = dayjs().format('YYYY-MM-DD');
-
   ngOnInit() {
     this._DoctorsService.renderDoctors().subscribe((res) => {
       this.doctorsCount = res.length;
     });
-
     this._PatientService.getAllPatients().subscribe((res) => {
       this.patientsCount = res.length;
     });
-// needs to be understood
+    this._AppointmentService.renderAppointments().subscribe((res) => {
+      this.appointmentsCount = res.length;
+      this.todayAppointments = res.filter((a) => {
+        return a.appointmentDate === this.td;
+      });
+    });
     this._AppointmentService
       .renderAppointments()
       .pipe(
-        switchMap((appointments) => {
-          this.appointmentsCount = appointments.length;
-          const todayAppointments = appointments.filter(
+        switchMap((app) => {
+          this.appointmentsCount = app.length;
+          const todayAppointments = app.filter(
             (a) => a.appointmentDate === this.td,
           );
-
           if (todayAppointments.length === 0) return of([]);
-
           return forkJoin(
             todayAppointments.map((a) =>
               forkJoin({
