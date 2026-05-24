d4e9432c8558bf0dcd4e0f1707d978f1cb5c9b3a
Kareem Khalifa <karemkhalifaa@gmail.com>
2026-05-25 00:07:06 +0300

    Fix Netlify publish directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgChartsModule } from 'ng2-charts';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgChartsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Health-Sync';
-
+  title = 'health-sync';
 }
