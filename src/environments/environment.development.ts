aa3886c2334515620edb58a244fe60257acc9c6d
Kareem Khalifa <karemkhalifaa@gmail.com>
2026-06-17 14:41:25 +0300

    fixed clinic id issue

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://zqxdqqjzowelpxgsldhq.supabase.co',
-  clinicId: 'e2905e4a-4f5c-472c-8831-aa6ca13cb53a',
+  clinicId: '9c0543f8-e8ef-41a6-8326-d636408def73',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxeGRxcWp6b3dlbHB4Z3NsZGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyODMzNzAsImV4cCI6MjA5Njg1OTM3MH0.azQGG8kFKUgBt2AsvRVrjUkKAQjsclOh7CChVDmH3RU'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://zqxdqqjzowelpxgsldhq.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxeGRxcWp6b3dlbHB4Z3NsZGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyODMzNzAsImV4cCI6MjA5Njg1OTM3MH0.azQGG8kFKUgBt2AsvRVrjUkKAQjsclOh7CChVDmH3RUDMzNzAsImV4cCI6MjA5Njg1OTM3MH0.azQGG8kFKUgBt2AsvRVrjUkKAQjsclOh7CChVDmH3RU',
-  clinicId: 'e2905e4a-4f5c-472c-8831-aa6ca13cb53a' 
+  clinicId: '9c0543f8-e8ef-41a6-8326-d636408def73' 
 };
