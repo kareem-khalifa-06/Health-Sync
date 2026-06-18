97ba646b7f6ffaee62a0d228ae11694fd6f6d2df
Kareem Khalifa <karemkhalifaa@gmail.com>
2026-06-18 03:59:00 +0300

    Added loading status for all async functions

--- src/app/core/services/app-state.service.ts
+++ src/app/core/services/app-state.service.ts
@@ -1,10 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AppStateService {
-  startLoader=signal<boolean>(false);
-  endLoader=signal<boolean>(false);
-  constructor() { }
+  startLoader = signal<boolean>(false);
 }
